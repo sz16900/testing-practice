a846e875b08c17a49ff78b9459211afe5ffd3ef3
sz16900 <sz16900@my.bristol.ac.uk>
2020-07-14 17:40:49 -0500

    :white_check_mark: capitalize
    write tests for capitalize unit

---
+++ src/capitalize.js
@@ -0,0 +1,6 @@
+const capitalize = (str) => {
+  if (typeof str !== 'string') return '';
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+export default capitalize;
---
+++ tests/capitalize.test.js
@@ -0,0 +1,13 @@
+import capitalize from '../src/capitalize';
+
+it('Capitalizes', () => {
+  expect(capitalize('seth')).toBe('Seth');
+});
+
+it('Returns only one capitalized letter', () => {
+  expect(capitalize('s')).toBe('S');
+});
+
+it('Returns nothing', () => {
+  expect(capitalize(8079876789)).toBe('');
+});
